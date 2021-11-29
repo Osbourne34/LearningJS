ac7f26878599566f101a0d9c46ef9a82d8651680
Osbourne34 <evolkov1804@gmail.com>
2021-11-29 20:23:51 +0500

    home work closures

---
+++ 6 Урок - Замыкание/7.js
@@ -0,0 +1,18 @@
+// 7) Какой результат будет выведен при очередном вызове generation и generation2? Почему?
+ 
+
+function numberGenerator() {
+  var currentNum = 2;
+  return function multNumber() {      
+    return currentNum *= currentNum;
+  };
+}
+
+var generation = numberGenerator();   
+var generation2 = numberGenerator();  
+
+alert( generation() ); // 4     2 * 2 = 4 * 4 = 16   
+alert( generation() ); // 16         
+
+alert( generation2() ); // 4    2 * 2 = 4 * 4 = 16       
+alert( generation2() ); // 16
