7869312261e124e6c185e4a169e03faa5076667b
Osbourne34 <evolkov1804@gmail.com>
2021-12-24 19:35:25 +0500

    testing-app

---
+++ testing-app/questions.js
@@ -0,0 +1,24 @@
+export const physics = [
+    {
+        question: 'Вопрос 1',
+        answers: {
+            a: 'A. Ответ',
+            b: '\nB. Ответ',
+            c: '\nC. Ответ',
+            d: '\nD. Ответ',
+        },
+        answer: 'b',
+    },
+    {
+        question: 'Вопрос 2',
+        answers: {
+            a: 'A. Ответ',
+            b: '\nB. Ответ',
+            c: '\nC. Ответ',
+            d: '\nD. Ответ',
+        },
+        answer: 'c',
+    },
+];
+
+
