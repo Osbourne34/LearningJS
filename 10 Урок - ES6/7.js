5133ecb0cb22fe482fcc6aa793f916f052601446
Osbourne34 <evolkov1804@gmail.com>
2021-12-27 18:24:56 +0500

    deleted files

--- 10 Урок - ES6/7.js
+++
@@ -1,9 +0,0 @@
-// 7)  Дан массив, выведите его элементы последовательно на экран.
-
-let arr = [1,2,3,4,5,7,8];
-
-arr.forEach(elem => console.log(elem));
-
-for(elem of arr) {
-    console.log(elem);
-}
