b100d0c03e9134fc5ed8c8d47838106d6b5cb35f
Osbourne34 <evolkov1804@gmail.com>
2022-01-20 21:39:07 +0500

    finished countries

--- countries/getdata.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://api.sampleapis.com/countries/countries';
-
-const getData = async(url) => {
-    const request = await fetch(url);
-    const data = await request.json();
-    return data;
-};
-
-export const data = getData(url);   
