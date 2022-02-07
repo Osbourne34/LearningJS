e190bdf76f942a48f699330d2bf57a0d3ccfd8f5
Osbourne34 <evolkov1804@gmail.com>
2022-02-07 20:28:23 +0500

    slider

--- slider-1/main.js
+++ slider-1/main.js
@@ -1,46 +1,67 @@
 class Slider {
-    constructor(slider, option) {
+    constructor(slider, countItems) {
         this.slider = document.querySelector(slider);
-        this.option = option;
+        this.countItems = countItems;
         this.init();
     }
 
-    createSliderLine(slider) {
-        let html = slider.innerHTML;
-        let sliderLine = `<div class="slider__line"> ${html} </div>`
-        slider.innerHTML = sliderLine;
-    }
+    addSliderLine() {
+        const items = Array.from(this.slider.children);
+        this.slider.innerHTML = `<div class="slider__track"></div>`
 
-    length() {
-        const sliderLine = document.querySelector('.slider__line');
-        const length = sliderLine.children.length;
-
-        return length;
-    }
-
-    setSliderLineWidth(length) {
-        document.querySelector('.slider__line').style.width = `${this.slider.clientWidth * length}px`
-    }
-
-    createDots(length) {
-        const ul = document.createElement('ul');
-        ul.classList = 'slider__dots'
-
-        for(let i = 0; i < length; i++) {
-            const li = document.createElement('li');
-            li.classList = 'slider__dots-btn'
-            ul.append(li)
+        for (let item of items) {
+            this.slider.children[0].append(item);
         }
 
-        this.slider.append(ul);
+        return document.querySelector('.slider__track');
+    }
+
+    setWidthSliderItems(sliderTrack, countItems) {
+
+        for (let sliderItem of sliderTrack.children) {
+            sliderItem.style.width = `${sliderTrack.offsetWidth / countItems}px`
+            sliderItem.style.flexShrink = 0;
+        }
+    }
+
+    createDots(countItems) {
+        const lengthItems = document.querySelector('.slider__track').children.length;
+        const dotsList = document.createElement('ul');
+        dotsList.classList = 'slider__dots-list';
+
+        for (let i = 0; i < lengthItems / countItems; i++) {
+            const dotsItem = document.createElement('li');
+            dotsItem.classList = 'slider__dots-item';
+            if (i === 0) {
+                dotsItem.classList = 'slider__dots-item active';
+            }
+            dotsList.appendChild(dotsItem);
+        }
+
+        this.slider.appendChild(dotsList);
+
+        return document.querySelectorAll('.slider__dots-item');
+
+    }
+
+    moveSlider(dots, countItems) {
+        const sliderTrack = document.querySelector('.slider__track');
+        const sliderItem = sliderTrack.children[0];
+        dots.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                console.log(sliderItem.clientWidth);
+                sliderTrack.style.transform = `translateX(${-sliderItem.offsetWidth * index * countItems}px)`
+                dots.forEach(dots => dots.classList.remove('active'));
+                item.classList.add('active');
+            });
+        });
     }
 
     init() {
-        this.createSliderLine(this.slider);
-        this.setSliderLineWidth(this.length());
-        this.createDots(this.length())
+        this.setWidthSliderItems(this.addSliderLine(), this.countItems);
+        this.moveSlider(this.createDots(this.countItems), this.countItems);
     }
-
 }
 
-const slider = new Slider('.slider');
+const slider = new Slider('.slider', 4);
+
