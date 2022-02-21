57a00e2bb9819bfa41bd02c6cdc5d56eae1a449c
Osbourne34 <evolkov1804@gmail.com>
2022-02-22 01:32:41 +0500

    todolist

--- projects/todoList/scripts/script.js
+++ projects/todoList/scripts/script.js
@@ -1,34 +1,57 @@
 class Todo {
 
-    constructor(date, todoValue, important, form, search, todoList) {
-        this.date = document.getElementById(date);
-        this.todoValue = document.getElementById(todoValue);
+    constructor(todoForm, inputDate, inputTitle, important) {
+        this.todoForm = document.getElementById(todoForm);
+        this.inputDate = document.getElementById(inputDate);
+        this.inputTitle = document.getElementById(inputTitle);
         this.important = document.getElementById(important);
-        this.form = document.getElementById(form)
-        this.search = document.getElementById(search);
-        this.todoList = document.getElementById(todoList);
 
         this.init();
     }
 
-    
-
     init() {
-        this.form.addEventListener('submit', (event) => {
+
+        const arr = [];
+
+        this.todoForm.addEventListener('submit', (event) => {
             event.preventDefault();
-            if(this.date.value === '') {
-                console.log('пусто');
-            } else {
-                console.log(this.date.value);
+
+            const obj = {
+                date: this.inputDate.value,
+                title: this.inputTitle.value,
+                important: this.important.checked,
             }
-            console.log(this.todoValue.value);
-            console.dir(this.important.checked);
-        })
-        localStorage.setItem('name', 'jenya');
-        console.log(localStorage.getItem('name'));
-        console.log(localStorage.length);
-        console.log(localStorage.name);
+
+
+            this.inputDate.value = ''
+            this.inputTitle.value = ''
+            this.important.checked = ''
+
+            arr.push(obj);
+
+            localStorage.setItem('task', JSON.stringify(arr));
+
+            let raw = localStorage.getItem('task');
+            raw = JSON.parse(raw);
+
+            const html = raw.map(item => {
+                return `
+                    <li class="todolist__item">
+                        <div class="todolist__left">
+                            <div class="todolist__status todolist__status_important"></div>
+                            <p class="todolist__text">${item.title}</p>
+                        </div>
+                        <div class="todolist__right">
+                            <div class="todolist__date">${item.date}</div>
+                            <button class="todolist__delete button button_large button_border">Delete</button>
+                            <button class="todolist__edit button button_large button_fill">Edit</button>
+                        </div>
+                    </li>
+                `
+            }).join('');
+            document.querySelector('.todolist__list').innerHTML = html;
+        });
     }
 }
 
-const todo = new Todo('date', 'todo-value', 'important', 'form', 'search', 'todo-list');
+const todo = new Todo('todo-form', 'input-date', 'input-title', 'important');
